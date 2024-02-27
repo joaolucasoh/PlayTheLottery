9b03eab0c09365dc32fa6dc435f9cc1222745053
Joao Lucas <joaolucas_oh@outlook.com>
2024-02-27 01:25:04 -0300

    Initial Commit

---
+++ PlayTheLottery/PlayTheLotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayTheLotteryApp.swift
+//  PlayTheLottery
+//
+//  Created by joaolucas on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayTheLotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
